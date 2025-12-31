580bbd8949da119790563cba4dcdcfc9b124e10b
Maximilian Bambauer <bambauer.max@gmail.com>
2025-12-31 11:09:38 +0100

    feat: Add multiplication and division functions with corresponding tests.

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -1,4 +1,5 @@
-from calculator.logic import add, subtract
+from calculator.logic import add, subtract, multiply, divide
+import pytest
 
 
 def test_addition():
@@ -11,3 +12,15 @@ def test_subtraction():
     assert subtract(10, 5) == 5
     assert subtract(-1, 1) == -2
     assert subtract(-1, -1) == 0
+
+
+def test_multiplication():
+    assert multiply(10, 5) == 50
+    assert multiply(-1, 1) == -1
+
+
+def test_division():
+    assert divide(10, 5) == 2
+    assert divide(-1, 1) == -1
+    with pytest.raises(ValueError, match="Cannot divide by zero"):
+        divide(1, 0)
