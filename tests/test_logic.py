565c24900e8b21a9cb8ea54a81bb33e5e6482a42
Maximilian Bambauer <bambauer.max@gmail.com>
2026-01-01 22:47:56 +0100

    chore: clean up the test cases.

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -4,14 +4,12 @@ import pytest
 
 def test_addition():
     assert add(10, 5) == 15
-    assert add(-1, 1) == 0
     assert add(-1, -1) == -2
 
 
 def test_subtraction():
     assert subtract(10, 5) == 5
     assert subtract(-1, 1) == -2
-    assert subtract(-1, -1) == 0
 
 
 def test_multiplication():
